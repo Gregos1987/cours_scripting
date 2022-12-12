402cb5e9748fadffc8fe60984d9fdc9e6dd45291
gregoire <gregviaud@hotmail.fr>
2022-12-12 10:41:26 +0100

    update

---
+++ bases.py
@@ -0,0 +1,6 @@
+chiffre = int(input())
+print(type (chiffre))
+if chiffre >= 0:
+    print("chiffre positif")
+else:
+    print("chiffre négatif")
--- exo fonction string retourne majuscule.py
+++ exo fonction string retourne majuscule.py
@@ -2,7 +2,46 @@ def majuscule(string):
     upper_count = 0
     str_char = "JgHf salut tout le monde !"
     for 4 in str_char:
+        upper_count += 1
         if upper_count >= 2
-        return str_char
+            return str_char
         else:
-        return str_char
+            return str_char
+    
+    
+    
+     #correction Athytian
+    
+    import sys
+def changestr(phrase):
+    maj_count = 0
+    fouthword=phrase[0:3]
+    for word in fouthword:
+        if word.isupper():
+            maj_count= maj_count+1
+    if maj_count >=2:
+        maj_phrase=phrase.upper()
+        print(maj_phrase)
+    else:
+        print(phrase)
+
+str_chain = sys.argv[1]
+changestr(str_chain)
+
+
+
+
+# correction
+def to_uppercase(string):
+  upper_count = 0
+  for i in range(4):
+    if string[i].isupper():
+      upper_count += 1
+  
+  if upper_count >= 2:
+    return string.upper()
+  else:
+    return string
+
+
+print(to_uppercase("Hello wORld"))
