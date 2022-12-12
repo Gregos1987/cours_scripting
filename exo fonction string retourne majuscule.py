828313e64833bfa2cf69c3dc770f29bdc5990643
gregoire <gregviaud@hotmail.fr>
2022-12-12 09:24:40 +0100

    update

---
+++ exo fonction string retourne majuscule.py
@@ -0,0 +1,8 @@
+def majuscule(string):
+    upper_count = 0
+    str_char = "JgHf salut tout le monde !"
+    for 4 in str_char:
+        if upper_count >= 2
+        return str_char
+        else:
+        return str_char
